95e711e91ac858467293025efc7dca8398d523dc
Hubert FU <63230660+NegativeCK@users.noreply.github.com>
2022-01-20 04:11:25 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import telegram
 import requests
 import json
-import schedule
+#import schedule
+from apscheduler.schedulers.blocking import BlockingScheduler
 from time import sleep
+
+sched = BlockingScheduler()
 
 
 def telegram_bot_sendtext(bot_message):
@@ -10,9 +13,8 @@ def telegram_bot_sendtext(bot_message):
     bot_token = ""
     bot_chatID = ""
 
-    f = open("config.txt")
-    bot_token,bot_chatID=f.read().split()
-    f.close()
+    bot_token= "***"
+    bot_chatID= "***"
 
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
@@ -20,6 +22,7 @@ def telegram_bot_sendtext(bot_message):
     response.json()
 
 
+@sched.scheduled_job('cron', hour=4, minute=8)
 def check_cloudsea():
     r = requests.get("https://www.hko.gov.hk/out_photo/ascent/ascent_table.xml")
     data = json.loads(r.text)
@@ -38,11 +41,10 @@ def check_cloudsea():
                     available = False
     if available == True:
         telegram_bot_sendtext("Gogogogogogogogogogogogogogogogogogogogogogogogoogogogogogogogogogogogo")
+    else:
+        telegram_bot_sendtext("oh no next time")
         
     
-schedule.every().day.at("03:00").do(check_cloudsea)
-
-while True:
-    
-    schedule.run_pending()
-    sleep(1)
+#schedule.every().day.at("04:54").do(check_cloudsea)
+#schedule.run_pending()
+sched.start()
